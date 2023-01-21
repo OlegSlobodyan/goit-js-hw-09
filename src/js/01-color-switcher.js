8b3fa4b5c924fc134d30acc68ecb4fd901f2d72d
OlegSlobodyan <oleg.slobodyan1994@gmail.com>
2023-01-21 22:58:55 +0100

    hw-09-03

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,6 +1,7 @@
 let timerId = null;
 const NOTIFICATION_DELAY = 1000;
 let activeStatus = false;
+
 const refs = {
   startButton: document.querySelector('button[data-start]'),
   stopButton: document.querySelector('button[data-stop]'),
