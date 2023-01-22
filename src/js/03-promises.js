048b345a75e132626e6c8eab68378a5f64fc53df
OlegSlobodyan <oleg.slobodyan1994@gmail.com>
2023-01-22 03:22:06 +0100

    hw-09
    Виправив 3 завдання

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -2,12 +2,15 @@ import Notiflix from 'notiflix';
 const refs = {
   form: document.querySelector('.form'),
   submitButton: document.querySelector('.create-promises'),
+  // valueInput: document.querySelector('first-delay.')
 };
+// const delayValue = 0;
+// refs.valueInput.addEventListener('input', (e) =>
+// delayValue = e.target.value)
 const value = {};
 refs.form.addEventListener('input', e => {
   value[e.target.name] = Number(e.target.value);
 });
-console.log(value);
 
 let promiseCounter = 1;
 
@@ -26,9 +29,12 @@ refs.submitButton.addEventListener('click', () => {
           );
         });
 
-      if (promiseCounter >= value.amount) {
+      if (promiseCounter === value.amount) {
+        promiseCounter = 0;
+        value.delay = 0;
         clearTimeout(intervalId);
       }
+
       promiseCounter += 1;
       value.delay += value.step;
     }, value.step);
